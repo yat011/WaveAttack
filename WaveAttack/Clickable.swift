e65627e22bd85cb04991126cd27530e0110494da
yat <devyat192002@gmail.com>
2015-10-09 03:44:14 +0800

    menu button

---
+++ WaveAttack/Clickable.swift
@@ -0,0 +1,9 @@
+//
+//  Clickable.swift
+//  WaveAttack
+//
+//  Created by yat on 9/10/2015.
+//
+//
+
+import Foundation
---
+++ WaveAttack/Model/ButtonUI.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonUI.swift
+//  WaveAttack
+//
+//  Created by yat on 8/10/2015.
+//
+//
+
+import Foundation
